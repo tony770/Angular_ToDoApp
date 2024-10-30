c1f8c3a3a769acaef4f7c4df98534c066286f5ac
Tony <tonyzoudev@gmail.com>
2024-10-29 22:53:45 -0400

    added navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { ListContainerComponent } from './list-container/list-container.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListComponent, ListContainerComponent],
+  imports: [RouterOutlet, ListComponent, ListContainerComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
