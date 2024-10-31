e5178f43c6e20365b8425156075d02b9dff05a8a
Tony <tonyzoudev@gmail.com>
2024-10-30 22:37:01 -0400

    added home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ListComponent } from './list/list.component';
-import { ListContainerComponent } from './list-container/list-container.component';
+import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListComponent, ListContainerComponent, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
